3863aadcc6d21c2eb4d788789665c430baff842e
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-29 10:55:23 -0700

    added: Datbase connection to java program is added

--- src/com/capgemini/jdbc/JDBC_EmployeePayroll.java
+++ src/com/capgemini/jdbc/JDBC_EmployeePayroll.java
@@ -1,5 +1,31 @@
 package com.capgemini.jdbc;
 
-public class JDBC_EmployeePayroll {
+import java.sql.*;
+import java.util.Enumeration;
 
+public class JDBC_EmployeePayroll {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver is loaded");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("cannot find driver in classpath", e);
+		}
+		listDrivers();
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root",
+					"Jashwini@2298");
+			System.out.println("Connection Done..!!!" + con);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println(" " + driverClass.getClass().getName());
+		}
+	}
 }
