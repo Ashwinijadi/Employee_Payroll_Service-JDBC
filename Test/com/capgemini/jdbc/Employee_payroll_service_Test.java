1515e227b4d999b5baf3a256390cf9e7a5391a46
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-11-04 23:25:58 -0800

    added: multiple employees are added into payroll

--- Test/com/capgemini/jdbc/Employee_payroll_service_Test.java
+++ Test/com/capgemini/jdbc/Employee_payroll_service_Test.java
@@ -1,8 +1,9 @@
 package com.capgemini.jdbc;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import com.capgemini.jdbc.Employee_payroll_service.IOService;
@@ -10,89 +11,37 @@ import com.capgemini.jdbc.Employee_payroll_service.IOService;
 class Employee_payroll_service_Test {
 
 	@Test
-	public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
-		Employee_payroll_service employeePayrollService = new Employee_payroll_service();
-		List<Employee_payroll_Data> employeePayrollData = employeePayrollService
-				.readEmployeepayrollData(IOService.DB_IO);
-		System.out.println(employeePayrollData);
-		Assert.assertEquals(3, employeePayrollData.size());
-	}
-
-	@Test
-	public void givenDateRange_WhenRetrieved_ShouldMatchEmployeeCount() {
+	public void givenEmployeesAddedToDbShouldMatchEmpCount() {
+		Employee_payroll_Data[] arrayOfEmps = { new Employee_payroll_Data(0, "Navya", 100000.0, LocalDate.now(), "F"),
+				new Employee_payroll_Data(0, "Bill Gates", 100000.0, LocalDate.now(), "M"),
+				new Employee_payroll_Data(0, "Sunder", 100000.0, LocalDate.now(), "M"),
+				new Employee_payroll_Data(0, "Mark", 100000.0, LocalDate.now(), "M") };
 		Employee_payroll_service employeePayrollService = new Employee_payroll_service();
 		employeePayrollService.readEmployeepayrollData(IOService.DB_IO);
-		LocalDate startDate = LocalDate.of(2018, 01, 01);
-		LocalDate endDate = LocalDate.now();
-		List<Employee_payroll_Data> employeePayrollData = employeePayrollService
-				.readPayrollDataForRange(IOService.DB_IO, startDate, endDate);
-		Assert.assertEquals(3, employeePayrollData.size());
+		Instant start = Instant.now();
+		employeePayrollService.addEmployeeToPayrollIn(Arrays.asList(arrayOfEmps));
+		Instant end = Instant.now();
+		System.out.println("Duration without thread : " + Duration.between(start, end));
+		Assert.assertEquals(5, employeePayrollService.countEntries(IOService.DB_IO));
 	}
 
-	@Test
-	public void givenEmployeePayrollData_ShouldMatch_AverageSalary_GroupByGender() {
-		Employee_payroll_service employeePayrollService = new Employee_payroll_service();
-		Map<String, Double> avgSalaryByGender = employeePayrollService.readPayrollDataForAvgSalary(IOService.DB_IO);
-		Assert.assertTrue(avgSalaryByGender.get("M").equals(1800000.0) && avgSalaryByGender.get("F").equals(170000.0));
-	}
-
-	@Test
-	public void givenEmployeePayrollData_ShouldMatch_SumOfSalary_GroupByGender() {
-		Employee_payroll_service employeePayrollService = new Employee_payroll_service();
-		Map<String, Double> sumOfSalaryByGender = employeePayrollService.readPayrollDataForSumSalary(IOService.DB_IO);
-		Assert.assertTrue(
-				sumOfSalaryByGender.get("M").equals(5400000.0) && sumOfSalaryByGender.get("F").equals(850000.0));
-	}
-
-	@Test
-	public void givenEmployeePayrollData_ShouldMatch_MaxSalary_GroupByGender() {
-		Employee_payroll_service employeePayrollService = new Employee_payroll_service();
-		Map<String, Double> MaxSalaryByGender = employeePayrollService.readPayrollDataForMaxSalary(IOService.DB_IO);
-		Assert.assertTrue(MaxSalaryByGender.get("M").equals(5000000.0) && MaxSalaryByGender.get("F").equals(350000.0));
-	}
-
-	@Test
-	public void givenEmployeePayrollData_ShouldMatch_MinSalary_GroupByGender() {
-		Employee_payroll_service employeePayrollService = new Employee_payroll_service();
-		Map<String, Double> MinSalaryByGender = employeePayrollService.readPayrollDataForMinSalary(IOService.DB_IO);
-		Assert.assertTrue(MinSalaryByGender.get("M").equals(100000.0) && MinSalaryByGender.get("F").equals(100000.0));
-	}
-
-	@Test
-	public void givenEmployeePayrollData_ShouldMatch_Emp_CountGroupByGender() {
-		Employee_payroll_service employeePayrollService = new Employee_payroll_service();
-		Map<String, Double> employeeCountByGender = employeePayrollService
-				.readPayrollDataFor_CountOfEmployee_ByGender(IOService.DB_IO);
-		Assert.assertTrue(employeeCountByGender.get("M").equals(3.0) && employeeCountByGender.get("F").equals(5.0));
-	}
-
-	@Test
-	public void givenNewSalaryForEmployeeWhenupdatedShouldSyncWith_NewNormalisedDB() throws EmployeePayrollException {
-		Employee_payroll_service employeePayrollService = new Employee_payroll_service();
-		List<Employee_payroll_Data> employeePayrollData = employeePayrollService
-				.readEmployeepayrollData(IOService.DB_IO);
-		employeePayrollService.updateEmployeeSalary("Hima", 300000.0);
-		boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Hima");
-		Assert.assertTrue(result);
-		System.out.println(employeePayrollData);
-	}
-
-	@Test
-	public void givenNewEmployeesWhenAddedInNormalised_ShouldSyncWithDB() {
-		Employee_payroll_service employeePayrollService = new Employee_payroll_service();
-		employeePayrollService.readEmployeepayrollData(IOService.DB_IO);
-		employeePayrollService.addEmployeeToPayRoll("Revanth", 5000000.00, LocalDate.now(), "M");
-		boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Revanth");
-		Assert.assertTrue(result);
-	}
-
-	@Test
-	public void givenEmployeePayrollData_ShouldReturn_ActiveEmployees() {
-		Employee_payroll_service employeePayrollService = new Employee_payroll_service();
-		employeePayrollService.readEmployeepayrollData(IOService.DB_IO);
-		List<Employee_payroll_Data> employeePayrollData = employeePayrollService
-				.readPayrollDataForActiveEmployees(IOService.DB_IO);
-		System.out.println(employeePayrollData);
-		Assert.assertEquals(5, employeePayrollData.size());
-	}
+//	@Test
+//	public void addingAddresses_WhenAddedToDB_ShouldMatchEmployeeEntries() {
+//		LocalDate date = LocalDate.now();
+//		Employee_payroll_Data arrayOfEmps[] = { new Employee_payroll_Data(0, "Navya", 100000.0, date, "F"),
+//				new Employee_payroll_Data(0, "Bill Gates", 100000.0, date, "M"),
+//				new Employee_payroll_Data(0, "Sunder", 200000.0, date, "M"),
+//				new Employee_payroll_Data(0, "Mark", 300000.0, date, "M") };
+//		Employee_payroll_service employeePayrollService = new Employee_payroll_service();
+//		employeePayrollService.readEmployeepayrollData(IOService.DB_IO);
+//		Instant start = Instant.now();
+//		employeePayrollService.addEmployeeToPayrollIn(Arrays.asList(arrayOfEmps));
+//		Instant end = Instant.now();
+//		System.out.println("Duration without thread : " + Duration.between(start, end));
+//		Instant threadStart = Instant.now();
+//		employeePayrollService.addEmployeeToPayrollWithThreads(Arrays.asList(arrayOfEmps));
+//		Instant threadEnd = Instant.now();
+//		System.out.println("Duartion with Thread : " + Duration.between(threadStart, threadEnd));
+//		Assert.assertEquals(8, employeePayrollService.countEntries(IOService.DB_IO));
+//	}
 }
